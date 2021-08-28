106232ccae4b21e0ae63c9de24dda918d120a168
Carlos Rodriguez <crodriguez3753@gmail.com>
2021-08-28 02:37:54 -0400

    edited card display

--- react-portfolio/src/components/CardItem.js
+++ react-portfolio/src/components/CardItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function CardItem() {
+function CardItem(props) {
     return (
         <>
-            <li className="cards_item">
-                <Link className="cards_item_link">
-                    <figure className="cards_item)pic-wrap">
-                        <img src="/" alt="Travel Image"
-                        className="cards_item_img"/>
+            <li className='cards__item'>
+                <Link className='cards__item__link' to={props.path}>
+                    <figure className='cards__item__pic-wrap' data-category={props.label}>
+                        <img src={props.src} alt='Travel' className='cards__item__img'
+                        />
                     </figure>
-                    <div className="cards_item_info">
-                        <h5 className="cards_item_text"/>
+                    <div className='cards__item__info'>
+                        <h5 className='cards__item__text'>{props.text}</h5>
                     </div>
                 </Link>
             </li>
@@ -19,4 +19,4 @@ function CardItem() {
     )
 }
 
-export default CardItem
+export default CardItem;
